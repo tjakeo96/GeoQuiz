b91983f96438e0a1162a8fcb9f60fef6dc5fcdbd
Jacob Newberry <jacobnewberry@Jacobs-MBP.frontierlocal.net>
2018-03-15 19:52:18 -0400

    Initial commit

---
+++ app/src/main/java/com/android/jacobnewberry/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.android.jacobnewberry.geoquiz;
+
+/**
+ * Created by jacobnewberry on 3/15/18.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
